042a54dd59aad9ddf66c5f51cf973e7f6a6c499d
kotetsu103 <jmfranco103@gmail.com>
2025-09-08 10:10:08 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'World'
+        name = 'Mundo'
     return 'Hola Cocas {}!'.format(name)
